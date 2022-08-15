9dba85eb23944218f096ccd3a4993e42335318b8
Ali Kazemi <alidownload2@yahoo.com>
2022-08-15 15:36:11 +0430

    first commit

---
+++ app/build/generated/source/buildConfig/debug/ir/ez4/helpiran/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ir.ez4.helpiran;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ir.ez4.helpiran";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
